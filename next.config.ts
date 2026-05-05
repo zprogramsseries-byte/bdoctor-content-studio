1c476b3f9a1871aab8f1b4fe647d7b7aa5cf4248
zprogramsseries-byte <zprogramsseries@gmail.com>
2026-05-05 01:42:43 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig;
